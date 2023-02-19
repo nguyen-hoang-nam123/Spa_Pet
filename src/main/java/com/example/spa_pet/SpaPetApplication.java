c92e8ef3f498624319a5f55a19656a83904e13b6
nguyen-hoang-nam123 <80116172+nguyen-hoang-nam123@users.noreply.github.com>
2023-02-19 17:32:56 +0700

    Initial commit

---
+++ src/main/java/com/example/spa_pet/SpaPetApplication.java
@@ -0,0 +1,13 @@
+package com.example.spa_pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaPetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spa_pet/SpaPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spa_pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaPetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
